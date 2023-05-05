917b7add304c15fb47d287fdd69c7a7d602c99bd
Vachek-hooves <vachek0602@gmail.com>
2023-05-05 21:27:57 +0300

    fix: absolute path implemented.

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,10 +2,7 @@ const fs = require('fs');
 const path= require('path');
 const fileLocation=path.join(__dirname, 'text.txt');
 
-console.log(fileLocation)
-
 const toRead=fs.createReadStream(fileLocation);
 let textToRead=''; // in case file text is to big, it will be connected to this variable.
 toRead.on('data', chunk=> console.log(textToRead+= chunk))
-// toRead.on('end',()=> console.log(textToRead.length)) // to understand how text is big. This function is opational.
 toRead.on('error', error=>console.log(error.message))
